38fa8d7a86ccdb2f5e02d32c3978f91ccc307c30
Ryan Meneses <meneses.ryan.j@gmail.com>
2021-11-19 10:38:33 -0800

    Add Place Description view

--- AndroidApp/app/src/main/java/edu/asu/bsse/rmenese1/androidapp/MainActivity.java
+++ AndroidApp/app/src/main/java/edu/asu/bsse/rmenese1/androidapp/MainActivity.java
@@ -3,6 +3,10 @@ package edu.asu.bsse.rmenese1.androidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView descriptionTextView = findViewById(R.id.descriptionTextView);
+        descriptionTextView.setMovementMethod(new ScrollingMovementMethod());
+
+        final TextView addressStreetTextView = findViewById(R.id.addressStreetTextView);
+        addressStreetTextView.setMovementMethod(new ScrollingMovementMethod());
+
+        final Button nameButton = findViewById(R.id.nameButton);
+        nameButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
     }
 }
