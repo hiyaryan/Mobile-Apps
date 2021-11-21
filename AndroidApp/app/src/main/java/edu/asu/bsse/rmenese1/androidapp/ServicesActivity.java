812d0554d7c9cd6addab58a04153ea1ac41afceb
Ryan Meneses <meneses.ryan.j@gmail.com>
2021-11-20 20:28:13 -0800

    Add services view

---
+++ AndroidApp/app/src/main/java/edu/asu/bsse/rmenese1/androidapp/ServicesActivity.java
@@ -0,0 +1,93 @@
+package edu.asu.bsse.rmenese1.androidapp;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Alert Activity (AlertActivity.java)
+ * This is the alert activity controller.
+ *
+ * @author Ryan Meneses
+ * @version 1.0
+ * @since November 19, 2021
+ */
+public class ServicesActivity extends AppCompatActivity {
+    /**
+     * onCreate
+     * This method initializes the view.
+     * @param savedInstanceState Bundle
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_services);
+
+        Intent intent = getIntent();
+        String value = intent.getStringExtra("ServicesActivity");
+        System.out.println(value);
+    }
+
+    /**
+     * onRestart
+     * This method is called when the app is opened from the background.
+     */
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        android.util.Log.d("LifeCycleMethod", this.getClass().getSimpleName() + ": " + "onRestart");
+    }
+
+    /**
+     * onStart
+     * This method is called when the view is called into the foreground.
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        android.util.Log.d("LifeCycleMethod", this.getClass().getSimpleName() + ": " + "onStart");
+    }
+
+    /**
+     * onResume
+     * This method is called when the view returns to the foreground.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        android.util.Log.d("LifeCycleMethod", this.getClass().getSimpleName() + ": " + "onResume");
+    }
+
+    /**
+     * onPause
+     * This method is called when the view is set to change.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        android.util.Log.d("LifeCycleMethod", this.getClass().getSimpleName() + ": " + "onPause");
+    }
+
+    /**
+     * onStop
+     * This method is called when the view has changed.
+     */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        android.util.Log.d("LifeCycleMethod", this.getClass().getSimpleName() + ": " + "onStop");
+    }
+
+    /**
+     * onDestroy
+     * This method is called when the app is closed.
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        android.util.Log.d("LifeCycleMethod", this.getClass().getSimpleName() + ": " + "onDestroy");
+    }
+}
