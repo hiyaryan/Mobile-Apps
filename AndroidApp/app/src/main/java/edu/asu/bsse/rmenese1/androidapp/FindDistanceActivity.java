bb40cba3d0ec2b7acf5e1ef62835a2a2a8fd2bb7
Ryan Meneses <meneses.ryan.j@gmail.com>
2021-11-22 13:02:01 -0800

    Add activity distance view

--- AndroidApp/app/src/main/java/edu/asu/bsse/rmenese1/androidapp/FindDistanceActivity.java
+++ AndroidApp/app/src/main/java/edu/asu/bsse/rmenese1/androidapp/FindDistanceActivity.java
@@ -2,10 +2,23 @@ package edu.asu.bsse.rmenese1.androidapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class FindDistanceActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class FindDistanceActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private JSONObject places;
+    private String key;
+
     /**
      * onCreate
      * This method initializes the view.
@@ -14,12 +27,82 @@ public class FindDistanceActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_services);
+        setContentView(R.layout.activity_distance);
 
+        android.util.Log.d("LifeCycleMethod", this.getClass().getSimpleName() + ": " + "onCreate");
+        
         Intent intent = getIntent();
-        String key = intent.getStringExtra("FindDistanceActivity");
+        Bundle extras = intent.getBundleExtra("FindDistanceActivity");
 
-        android.util.Log.d("LifeCycleMethod", this.getClass().getSimpleName() + ": " + "onCreate"
-                + " -> " + key);
+        this.key = extras.getString("key");
+        String places = extras.getString("places");
+
+        try {
+            this.places = new JSONObject(places);
+
+        } catch (JSONException je) {
+            android.util.Log.d("Error", this.getClass().getSimpleName() + ": "
+                    + "Could not parse JSON.");
+        }
+
+        // Create a Set the Spinner with an array of Places
+        Spinner fromPlaceSpinner = findViewById(R.id.fromPlaceSpinner);
+        fromPlaceSpinner.setOnItemSelectedListener(this);
+        setSpinnerElements(fromPlaceSpinner);
+
+        // Create a Set the Spinner with an array of Places
+        Spinner toPlaceSpinner = findViewById(R.id.toPlaceSpinner);
+        toPlaceSpinner.setOnItemSelectedListener(this);
+        setSpinnerElements(toPlaceSpinner);
+    }
+
+    /**
+     * Implement spinner element select listener.
+     *
+     * @param parent AdapterView
+     * @param view View
+     * @param position int
+     * @param id long
+     */
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        android.util.Log.d("Spinner", this.getClass().getSimpleName()
+                + ": " + "onItemSelected "
+                + "-> " + parent.getItemAtPosition(position));
+    }
+
+    /**
+     * Implement spinner element no select listener.
+     *
+     * @param parent AdapterView
+     */
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        android.util.Log.d("Spinner", this.getClass().getSimpleName() + ": " + "onNothingSelected");
+    }
+
+
+    /**
+     * Set the keys of places.json as the elements to display in spinner.
+     * @param spinner Spinner
+     */
+    private void setSpinnerElements(Spinner spinner) {
+        android.util.Log.d("Spinner", this.getClass().getSimpleName() + ": " + "setSpinnerElements");
+
+        ArrayList<String> elements = new ArrayList<>();
+        for (Iterator<String> it = places.keys(); it.hasNext(); ) {
+            String place = it.next();
+            elements.add(place);
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, elements);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        // The fromSpinner should start with the place presented on the main view
+        if (spinner.getId() == R.id.fromPlaceSpinner) {
+            int spinnerPosition = adapter.getPosition(this.key);
+            spinner.setSelection(spinnerPosition);
+        }
     }
 }
