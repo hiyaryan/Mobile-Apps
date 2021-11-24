5860eb6bf8451e29506f142dbaa0149fc2c9df34
Ryan Meneses <meneses.ryan.j@gmail.com>
2021-11-24 10:36:49 -0800

    Add database schema and helper class

---
+++ AndroidApp/app/src/main/java/edu/asu/bsse/rmenese1/androidapp/PlacesContract.java
@@ -0,0 +1,62 @@
+package edu.asu.bsse.rmenese1.androidapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+public final class PlacesContract {
+    private static final String SQL_CREATE_PLACES =
+            "CREATE TABLE " + PlacesEntry.TABLE_NAME + " ("
+            + PlacesEntry._ID + " INTEGER PRIMARY KEY,"
+            + PlacesEntry.COLUMN_NAME_NAME + " TEXT,"
+            + PlacesEntry.COLUMN_NAME_DESCRIPTION + " TEXT,"
+            + PlacesEntry.COLUMN_NAME_CATEGORY + " TEXT,"
+            + PlacesEntry.COLUMN_NAME_ADDRESS_TITLE + " TEXT,"
+            + PlacesEntry.COLUMN_NAME_ADDRESS_STREET + " TEXT,"
+            + PlacesEntry.COLUMN_NAME_ELEVATION + " INTEGER,"
+            + PlacesEntry.COLUMN_NAME_LATITUDE + " INTEGER,"
+            + PlacesEntry.COLUMN_NAME_LONGITUDE + " INTEGER)";
+
+    private static final String SQL_DELETE_PLACES =
+            "DROP TABLE IF EXISTS " + PlacesEntry.TABLE_NAME;
+
+    private PlacesContract() {}
+
+    public static class PlacesEntry implements BaseColumns {
+        public static final String TABLE_NAME = "place";
+        public static final String COLUMN_NAME_NAME = "name";
+        public static final String COLUMN_NAME_DESCRIPTION = "description";
+        public static final String COLUMN_NAME_CATEGORY = "category";
+        public static final String COLUMN_NAME_ADDRESS_TITLE = "address_title";
+        public static final String COLUMN_NAME_ADDRESS_STREET = "address_street";
+        public static final String COLUMN_NAME_ELEVATION = "elevation";
+        public static final String COLUMN_NAME_LATITUDE = "latitude";
+        public static final String COLUMN_NAME_LONGITUDE = "longitude";
+    }
+
+    public static class PlacesDbHelper extends SQLiteOpenHelper {
+        public static final int DATABASE_VERSION = 1;
+        public static final String DATABASE_NAME = "Places.db";
+
+        public PlacesDbHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(SQL_CREATE_PLACES);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL(SQL_DELETE_PLACES);
+            onCreate(db);
+        }
+
+        @Override
+        public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            onUpgrade(db, oldVersion, newVersion);
+        }
+    }
+}
